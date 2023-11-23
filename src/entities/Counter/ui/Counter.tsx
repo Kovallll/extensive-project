48d554e221647c622123b35dd9e06de348d3823a
KoVaL <kovallll.e@gmail.com>
2023-11-23 22:28:45 +0300

    add modal, add input, add loginModal

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,12 +1,13 @@
-import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'shared/ui/Button/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { counterActions } from '../model/slice/counterSlice';
 import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
-import { counterActions } from '../model/slice/CounterSlice';
 
 export const Counter = () => {
-
     const dispatch = useDispatch();
     const counterValue = useSelector(getCounterValue);
+    const { t } = useTranslation();
 
     const increment = () => {
         dispatch(counterActions.increment());
@@ -18,18 +19,18 @@ export const Counter = () => {
 
     return (
         <div>
-            <h1>
-                value = {counterValue}
-            </h1>
+            <h1 data-testid="value-title">{counterValue}</h1>
             <Button
                 onClick={increment}
+                data-testid="increment-btn"
             >
-                increment
+                {t('increment')}
             </Button>
             <Button
+                data-testid="decrement-btn"
                 onClick={decrement}
             >
-                decrement
+                {t('decrement')}
             </Button>
         </div>
     );
